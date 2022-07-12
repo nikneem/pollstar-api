c735c566bf3b78938abfb7b6a1e24fb3b8c60b38
Eduard <e.keilholz@4dotnet.nl>
2022-07-12 15:53:04 +0200

    Adding values

--- src/PollStar.Core/EnvironmentVariableName.cs
+++ src/PollStar.Core/EnvironmentVariableName.cs
@@ -2,5 +2,6 @@
 
 public class EnvironmentVariableName
 {
-    
+    public const string AzureStorageAccountName = "Azure_StorageAccount";
+    public const string AzureStorageAccountKey = "Azure_StorageKey";
 }
